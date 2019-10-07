92fc80deff0388125b3d0e3bb97b207627a202cc
Nikita Nakonechnyi <whofarted@mail.ru>
2019-10-07 23:39:29 +0300

    solvation

---
+++ src/LuckyFloor.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+public class LuckyFloor {
+
+    public static void main(String[] args) {
+        System.out.println(getLuckyFloor(1344678));
+
+    }
+
+    private static int getLuckyFloor(int realFloor) {
+
+        int luckyFloor = 0;
+        for (int i = 1; i <= realFloor; i++) {
+            luckyFloor++;
+            while (!isLucky(luckyFloor)) {
+                luckyFloor++;
+            }
+        }
+        return luckyFloor;
+    }
+
+
+    private static boolean isLucky(int floor) {
+        String string = Integer.toString(floor);
+
+        for (int i = 0; i < string.length(); i++) {
+            if (string.contains("13") || string.contains("4")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
