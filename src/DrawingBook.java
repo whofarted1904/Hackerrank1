561ecf856063263b1661e7b8c533769b55cae368
Nikita Nakonechnyi <whofarted@mail.ru>
2019-10-09 02:23:48 +0300

    solvation

---
+++ src/DrawingBook.java
@@ -0,0 +1,23 @@
+public class DrawingBook {
+
+    public static void main(String[] args) {
+        System.out.println(pageCount(99, 94));
+
+    }
+
+    public static int pageCount(int n, int p) {
+//
+//        int pageCountFront = 0;
+//        int pageCountBack = 0;
+//        for (int i = 2; i < p; i = i + 1) {
+//            pageCountFront++;
+//        }
+//        for (int i = n - 1; i < p; i = i - 1) {
+//            pageCountBack++;
+//        }
+//
+//        return Math.min(pageCountFront, pageCountBack);
+        return Math.min(p/2, n/2-p/2);
+    }
+
+}
